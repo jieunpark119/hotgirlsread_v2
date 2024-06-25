487889238c077ac3b216bd195e87935eb86b787c
Ji <jieunpark119@gmail.com>
2024-06-25 16:04:39 -0400

    back to scroll magic

--- public/script.js
+++ public/script.js
@@ -1,19 +1,19 @@
-// (function () {
-//   var controller = new ScrollMagic.Controller({
-//     globalSceneOptions: {
-//       triggerHook: "onLeave",
-//       duration: "100%", // Adjust the duration as needed
-//     },
-//   });
+(function () {
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: "onLeave",
+      duration: "100%", // Adjust the duration as needed
+    },
+  });
 
-//   var slides = document.querySelectorAll("section.panel");
+  var slides = document.querySelectorAll("section.panel");
 
-//   slides.forEach(function (slide) {
-//     new ScrollMagic.Scene({
-//       triggerElement: slide,
-//     })
+  slides.forEach(function (slide) {
+    new ScrollMagic.Scene({
+      triggerElement: slide,
+    })
 
-//       .setPin(slide, { pushFollowers: false })
-//       .addTo(controller);
-//   });
-// })();
+      .setPin(slide, { pushFollowers: false })
+      .addTo(controller);
+  });
+})();
